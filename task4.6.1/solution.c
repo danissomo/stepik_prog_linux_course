a397f9c56131ea8fd41343fa07f704d39915b6ef
danissomo <dan.2k64@gmail.com>
2020-08-27 23:26:03 +0000

    первая версия решения task4.5.1

---
+++ task4.6.1/solution.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#define MAX 8192
+int main(int argc, char* argv[]){
+    char com[128];
+    sprintf("%s %s", argv[1], argv[2]);
+    
+    FILE * fd  = popen(com, "r");
+    char tmp;
+    long int count= 0;
+    while((tmp = fgetc(fd)) != EOF )
+        if(tmp == '0') count++;
+    pclose(fd);
+    printf("%ld\n", count);
+
+}
