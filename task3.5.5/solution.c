6ac2c29c7e272e766e98426719fe3d06330a5f4c
danissomo <dan.2k64@gmail.com>
2020-08-31 11:28:43 +0000

    task3.5.5 добавил к демону обработку сигнала завершения

--- task3.5.5/solution.c
+++ task3.5.5/solution.c
@@ -1,8 +1,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void sig_hndlr(int signo){
+    exit(EXIT_SUCCESS);
+}
 
 int main(){
+    signal(SIGURG, sig_hndlr);
     printf("%d\n", getpid());
     setsid();
     close(STDIN_FILENO);
