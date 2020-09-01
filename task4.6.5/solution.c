f4043b476beeb76cdb9f9f8f321528299f2ccaa8
danissomo <dan.2k64@gmail.com>
2020-09-01 20:00:32 +0000

    atoill, ipc_creat | 0666

--- task4.6.5/solution.c
+++ task4.6.5/solution.c
@@ -5,14 +5,14 @@
 #include <sys/ipc.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <unistd.h>
 int main(int c, char *v[]){
     key_t key;
-    int fd =open("shm-c", O_CREAT);
-    close(fd);
-    key = ftok("shm-c", 0);
-    int* shmseg1 = (int*) shmat(atoi(v[1]), NULL, 0);
-    int* shmseg2 = (int*) shmat(atoi(v[2]), NULL, 0);
-    int shmid = shmget(key, sizeof(int)*100, IPC_CREAT | IPC_EXCL);
+    
+    key = ftok(v[0], getpid());
+    int* shmseg1 = (int*) shmat(atoll(v[1]), NULL, 0);
+    int* shmseg2 = (int*) shmat(atoll(v[2]), NULL, 0);
+    int shmid = shmget(key, sizeof(int)*100,  IPC_CREAT | 0666);
     int* rshmseg = (int*)shmat(shmid, NULL, 0);
     int i;
     for(i =0; i < 100; i++)
