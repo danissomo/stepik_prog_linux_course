63f4d945901d62dc22a076e85055e3de9c206a19
danissomo <dan.2k64@gmail.com>
2020-09-01 19:50:21 +0000

    task4.6.5

--- task4.6.5/solution.c
+++ task4.6.5/solution.c
@@ -3,18 +3,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <sys/ipc.h>
-
+#include <fcntl.h>
 int main(int c, char *v[]){
     key_t key;
-    int* shmseg1 = (int*) shmat(atoi(v[1]), NULL, SHM_RDONLY);
-    int* shmseg2 = (int*) shmat(atoi(v[2]), NULL, SHM_RDONLY);
-    int shmid = shmget(key, sizeof(int)*100, IPC_CREAT);
-    int* rshmseg = (int*)shmat(shmid, NULL, NULL);
+    int fd =open("shm-c", O_CREAT);
+    close(fd);
+    key = ftok("shm-c", 0);
+    int* shmseg1 = (int*) shmat(atoi(v[1]), NULL, 0);
+    int* shmseg2 = (int*) shmat(atoi(v[2]), NULL, 0);
+    int shmid = shmget(key, sizeof(int)*100, IPC_CREAT | IPC_EXCL);
+    int* rshmseg = (int*)shmat(shmid, NULL, 0);
     int i;
     for(i =0; i < 100; i++)
         rshmseg[i] = shmseg1[i] + shmseg2[i];
     shmdt(shmseg2);
     shmdt(shmseg1);
-    printf("%d\n", shmid);
+    printf("%d\n", key);
     shmdt(rshmseg);
 }
