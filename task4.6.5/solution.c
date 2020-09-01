a798fcccd88f520d18206c5437a913168af33125
danissomo <dan.2k64@gmail.com>
2020-09-01 16:06:05 +0000

    task4.6.5

---
+++ task4.6.5/solution.c
@@ -0,0 +1,19 @@
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+
+int main(int c, char *v[]){
+    int* shmseg1 = (int*) shmat(atoi(v[1]), NULL, SHM_RDONLY);
+    int* shmseg2 = (int*) shmat(atoi(v[2]), NULL, SHM_RDONLY);
+    int shmid = shmget(2222, sizeof(int)*100, IPC_CREAT);
+    int* rshmseg = (int*)shmat(shmid, NULL, NULL);
+    int i;
+    for(i =0; i < 100; i++)
+        rshmseg[i] = shmseg1[i] + shmseg2[i];
+    shmdt(shmseg2);
+    shmdt(shmseg1);
+    printf("%d\n", shmid);
+    shmdt(rshmseg);
+}
